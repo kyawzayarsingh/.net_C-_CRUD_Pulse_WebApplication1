e623c89d59f8f4ead20c2b3ffbcb39ebc784ade3
kyawzayarsingh <63597619+kyawzayarsingh@users.noreply.github.com>
2022-10-10 16:55:04 +0630

    add signalr third library and add ChatRoom controller and view

---
+++ MVCCrudCodeFirst/MVCCrudCodeFirst/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCCrudCodeFirst
+{
+    public class ChatHub : Hub
+    {
+        //public void Hello()
+        //{
+        //    Clients.All.hello();
+        //}
+
+        public void Send(string name, string message)
+        {
+            //call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
---
+++ MVCCrudCodeFirst/MVCCrudCodeFirst/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(MVCCrudCodeFirst.Startup))]
+
+namespace MVCCrudCodeFirst
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
